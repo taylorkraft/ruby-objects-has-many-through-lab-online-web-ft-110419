da62ccc35bcdd5c5c2c9229f9a85e9a9fb043c10
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-21 22:16:16 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,15 +2,15 @@ class Appointment
   attr_accessor :date, :patient, :doctor
 
   @@all = []
-  
+
   def initialize(date, patient, doctor)
     self.date = date
     self.patient = patient
     self.doctor = doctor
     @@all << self
   end
-  
-  def self.all 
-    @@all 
+
+  def self.all
+    @@all
   end
 end
