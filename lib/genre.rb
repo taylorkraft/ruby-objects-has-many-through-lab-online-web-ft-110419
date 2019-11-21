822e6bbc644dd722de0719593f7a61440a29ece7
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-21 21:36:55 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,5 +1,16 @@
 class Genre 
-  
+  attr_accessor :name
+
+  def initialize(name)
+    self.name = name 
+  end 
+
+  def songs 
+   return Song.all.filter do |song|
+     song.genre == self
+   end
+  end 
+
   
   
   
