21d0d6b98d782ea6296a9b18dfdf6c68379e5a5d
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-21 21:42:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,16 @@
-class Song 
+class Song
   attr_accessor :genre, :artist, :name
 
   @@all = []
-  
+
   def initialize(name, artist, genre)
     self.name = name
     self.artist = artist
-    self.genre = genre 
-    @@all << self 
+    self.genre = genre
+    @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
 end
