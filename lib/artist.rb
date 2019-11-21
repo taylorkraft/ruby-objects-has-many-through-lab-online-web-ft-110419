5f17c12bd3f8851348721b1ae2a7ddf4a9720b64
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-21 21:17:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,30 @@
 class Artist 
-  
+  attr_accessor :name, :songs
 
+  @@all = []
   
+  def initialize(name)
+    self.name = name
+    @@all << self 
+  end
+
+  def self.all
+    @@all 
+  end 
+
+  def songs
+    return Song.all.filter do |song|
+      song.artist == self 
+    end
+  end
+
+  def new_song(name, genre)
+    return Song.new(name, self, genre)
+  end
+
+  def genres
+    self.songs.collect do |song|
+      song.genre 
+    end
+  end
 end
