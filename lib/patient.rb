25802e2d80764e11d6bb9c46adc523eff96d3530
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-21 22:43:12 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,10 +1,30 @@
 class Patient
-  
-  
-  
-  
-  
-  
-  
-  
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    self.name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, doctor)
+    return Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.filter do |apt|
+      apt.patient == self
+    end
+  end
+
+  def doctor
+    self.appointments.collect do |apt|
+      apt.doctor
+    end
+  end
 end
